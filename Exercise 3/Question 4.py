0f413d7c5d9822e566924d46a3a36815630f6903
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-10-26 08:08:31 +0700

    Create Question 4.py

---
+++ Exercise 3/Question 4.py
@@ -0,0 +1,11 @@
+# Question 4
+def calc_new_height():
+    width = float(input("Enter your current width: "))
+    height = float(input("Enter your current height: "))
+    desire = float(input("Enter your desired width: "))
+    curras = height/width
+    needhei = curras * desire
+
+    print("The corresponding height is: ", needhei )
+
+calc_new_height()
