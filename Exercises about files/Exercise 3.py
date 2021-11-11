4e1fecf65f44ead9bd3354bddc67737d68dc3bf4
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-11-11 16:14:19 +0700

    Create Exercise 3.py

---
+++ Exercises about files/Exercise 3.py
@@ -0,0 +1,34 @@
+import os
+
+
+os.chdir(r'C:\Users\ACER\Desktop')
+file = open("Links.txt", "r")
+book = file.readlines()
+punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+count = -1
+for i in book:
+    count += 1
+    for b in i:
+        if b in punc:
+            book.pop(count)
+            damn = i.replace(b, "")
+            book.insert(count,damn)
+
+
+words = 0
+for i in book:
+    word = i.split()
+    for werd in word:
+        words += 1
+
+letters = 0
+for i in book:
+    letter = i.split()
+    for wook in letter:
+        for bro in wook:
+            letters += 1
+
+print(f"Average word count of this book is {letters/words}")
+
+file.close()
