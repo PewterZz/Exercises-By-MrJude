94a8881ae2a3e20b5f62a1e74fc2ef0c1a2f946d
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-06 18:40:25 +0700

    Update PriceClassPNS.py

--- Exercise about classes/PriceClassPNS.py
+++ Exercise about classes/PriceClassPNS.py
@@ -1,9 +1,9 @@
 import ModulePNS
-#imported class from ModulePNS.py 
-#driver code
+# imported class from ModulePNS.py 
+# driver code
 def createlistPNS():
     objects = list()
-    #var used as a counting method for items
+    # var used as a counting method for items
     var = 1
     items = eval(input('How many items would you like ?, Must be at least 1: '))
     while items < 1:
@@ -18,22 +18,22 @@ def createlistPNS():
         while pounds <= 0:
             pounds = eval(input('Amount of item in pounds: '))
         items -= 1
-        #Creating the object and appending it onto a list
+        # Creating the object and appending it onto a list
         instance = Module.PricesPNS(itemname, pounds)
         objects.append(instance)
     return objects
-#Function to show the list in an ordered manner, for getting attributes i used getattr() and referenced the variables in the class, inside the list are the objects made from before.
+# Function to show the list in an ordered manner, for getting attributes i used getattr() and referenced the variables in the class, inside the list are the objects made from before.
 def showlistPNS(list):
     totalprice = 0
     print('Here\'s a summary of the items purchased:\n----------------------------\n')
-    #Looping through all objects appended into the list from before
+    # Looping through all objects appended into the list from before
     for i in list:
         i.checkitemPNS()
         print('\nitem:', getattr(i, 'dish'))
         print('Amount ordered: ', getattr(i, 'weight'), 'Pounds')
         print('Price per pound: ', '$'+str(getattr(i, 'price')))
         print('Price of order: ', '$'+str(i.totalcostPNS()))
-        #total price of all items added together for each object
+        # total price of all items added together for each object
         totalprice += i.totalcostPNS()
     print('\nTotal Cost: $'+str(totalprice))
 
