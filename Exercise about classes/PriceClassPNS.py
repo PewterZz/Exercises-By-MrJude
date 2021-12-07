9ff06dcd11f833b40d87495f844d36eabe799334
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-07 11:27:41 +0700

    Update PriceClassPNS.py

--- Exercise about classes/PriceClassPNS.py
+++ Exercise about classes/PriceClassPNS.py
@@ -1,18 +1,28 @@
-import ModulePNS
-# imported class from ModulePNS.py 
+import Module
+
+
+# imported class from ModulePNS.py
 # driver code
 def createlistPNS():
     objects = list()
     # var used as a counting method for items
     var = 1
-    items = eval(input('How many items would you like ?, Must be at least 1: '))
-    while items < 1:
-        items = eval(input('How many items would you like ?, MUST BE AT LEAST 1: '))
+    items = 0
+    while items == 0:
+        try:
+            items = eval(input('How many items would you like ?, Must be at least 1: '))
+            while items < 1:
+                items = eval(input('How many items would you like ?, MUST BE AT LEAST 1: '))
+            break
+        except:
+            print('Try Again')
+            items = 0
+
     while items != 0:
         itemname = "blank"
         pounds = 0
         while itemname == "blank":
-            print('item','#' + str(var))
+            print('item', '#' + str(var))
             itemname = input('Name of the item: ')
             var += 1
         while pounds <= 0:
@@ -22,6 +32,8 @@ def createlistPNS():
         instance = Module.PricesPNS(itemname, pounds)
         objects.append(instance)
     return objects
+
+
 # Function to show the list in an ordered manner, for getting attributes i used getattr() and referenced the variables in the class, inside the list are the objects made from before.
 def showlistPNS(list):
     totalprice = 0
@@ -31,12 +43,13 @@ def showlistPNS(list):
         i.checkitemPNS()
         print('\nitem:', getattr(i, 'dish'))
         print('Amount ordered: ', getattr(i, 'weight'), 'Pounds')
-        print('Price per pound: ', '$'+str(getattr(i, 'price')))
-        print('Price of order: ', '$'+str(i.totalcostPNS()))
+        print('Price per pound: ', '$' + str(getattr(i, 'price')))
+        print('Price of order: ', '$' + str(i.totalcostPNS()))
         # total price of all items added together for each object
         totalprice += i.totalcostPNS()
-    print('\nTotal Cost: $'+str(totalprice))
+    print('\nTotal Cost: $' + str(totalprice))
 
-    
+
 brother = createlistPNS()
 showlistPNS(brother)
+
