21465d08a1dd56a289d8d23081d303ef3ed9c301
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-08 09:02:54 +0700

    Update PriceClassPNS.py

--- Exercise about classes/PriceClassPNS.py
+++ Exercise about classes/PriceClassPNS.py
@@ -37,13 +37,13 @@ def createlistPNS():
 # Function to show the list in an ordered manner, for getting attributes i used getattr() and referenced the variables in the class, inside the list are the objects made from before.
 def showlistPNS(list):
     totalprice = 0
-    print('Here\'s a summary of the items purchased:\n----------------------------\n')
+    print('\nHere\'s a summary of the items purchased:\n----------------------------\n')
     # Looping through all objects appended into the list from before
     for i in list:
         i.checkitemPNS()
-        print('\nitem:', getattr(i, 'dish'))
-        print('Amount ordered: ', getattr(i, 'weight'), 'Pounds')
-        print('Price per pound: ', '$' + str(getattr(i, 'price')))
+        print('item:', i.getdishPNS())
+        print('Amount ordered: ', i.getweightPNS(), 'Pounds')
+        print('Price per pound: ', '$' + str(i.getpricePNS()))
         print('Price of order: ', '$' + str(i.totalcostPNS()))
         # total price of all items added together for each object
         totalprice += i.totalcostPNS()
@@ -52,4 +52,3 @@ def showlistPNS(list):
 
 brother = createlistPNS()
 showlistPNS(brother)
-
