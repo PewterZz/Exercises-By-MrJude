5bd5238936bafc7440b8f96fa042e84054663900
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-03-02 11:12:59 +0700

    Create Number12.java

---
+++ Java Exercises/Drill1/Number12.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Number  Square  Cube");
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf(" %d\t\t%d\t\t%d\n", i, i * i, i * i * i);
+        }
+    }
+}
