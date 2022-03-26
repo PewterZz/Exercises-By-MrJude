524dc0f65bc2afc2bee0ff8b4adfb5646492765b
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-03-26 15:18:19 +0700

    Create MachineProblem3.java

---
+++ ForumWeek2/MachineProblem3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class main2{
+    public static void main(String[] args){
+        double sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numStudents = input.nextInt();
+        int[] Scores = new int[numStudents];
+        for (int i = 0; i < numStudents; i++){
+            System.out.printf("Enter the grade for student %d: ", i+1);
+            int number = input.nextInt();
+            Scores[i] = number;
+        }
+
+        for (int i = 0; i < numStudents; i++){
+            sum += Scores[i];
+        }
+
+        double average = (sum/numStudents);
+        System.out.print("The average is: " + average);
+        System.out.println();
+
+        int[] Scores2 = new int[numStudents];
+        System.arraycopy(Scores, 0, Scores2, 0 , numStudents);
+        for (int i = 0; i+1 < numStudents; i++){
+            if(Scores[0] < Scores[i+1]){
+                Scores[0] = Scores[i+1];
+            }
+        }
+        System.out.printf("The maximum is: %d", Scores[0]);
+        System.out.println();
+
+        for (int i = 0; i+1 < numStudents; i++){
+            if(Scores2[0] > Scores2[i+1]){
+                Scores2[0] = Scores2[i+1];
+            }
+        }
+        System.out.printf("The minimum is: %d", Scores2[0]);
+
+
+    }
+}
