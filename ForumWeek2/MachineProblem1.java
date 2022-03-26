d245a6723c86e3b048b8950db47c98def90309c3
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-03-26 13:52:46 +0700

    Update MachineProblem1.java

--- ForumWeek2/MachineProblem1.java
+++ ForumWeek2/MachineProblem1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main2{
+public class PrintArray{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of items: ");
