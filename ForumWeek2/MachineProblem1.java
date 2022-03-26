7b8ba0b34cd27fc6bce5becc4c49f492fbaa909c
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-03-26 13:51:34 +0700

    Create MachineProblem1.java

---
+++ ForumWeek2/MachineProblem1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of items: ");
+        int NUM_ITEMS = input.nextInt();
+        int[] items = new int[NUM_ITEMS];
+        System.out.print("Enter the value of all items (seperated by space): ");
+        for(int i = 0; i < NUM_ITEMS; i++){
+            items[i] = input.nextInt();
+        }
+
+        System.out.print("The values are: [");
+        for(int i = 0; i < NUM_ITEMS; i++){
+            System.out.print(items[i]);
+            if(NUM_ITEMS > i+1) {
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
+
+
+    }
+}
