092739977d06d4df2849597bfc81bbd6f6f80792
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-05-11 15:16:56 +0700

    Create Customer.java

---
+++ ForumWeek3OOP/Customer.java
@@ -0,0 +1,33 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Account account;
+    private int password;
+
+    public Customer(String f, String l, int password){
+        this.firstName = f;
+        this.lastName = l;
+        this.password = password;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public Account getAccount(){
+        return this.account;
+    }
+
+    public void setAccount(Account acc){
+        this.account = acc;
+    }
+
+    public int getPassword(){
+        return this.password;
+    }
+
+}
